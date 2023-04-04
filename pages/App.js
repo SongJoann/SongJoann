cb27aafc3e2ecc9f13e15eca8c597aaeac31e71c
joann.song <joann.song@spotter.la>
2023-04-04 15:33:31 -0700

    remove cache

--- pages/App.js
+++ pages/App.js
@@ -1,13 +1,13 @@
-import "../styles/App.css";
+import "/src/App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="/public/assets/JoannSongLogo.png" width="500" height="500" alt="logo" />
+        <img src="./public/assets/JoannSongLogo.png" width="500" height="500" alt="logo" />
         <a
           className="Main Page"
-          href="index.html"
+          href="/public/index.html"
         >
           Enter My Website
         </a>
