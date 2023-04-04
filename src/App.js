d262674e3b2ff7047f30c4da1ba3aa736095ebd6
joann.song <joann.song@spotter.la>
2023-04-04 16:06:54 -0700

    moved everything to src:

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="../public/assets/JoannSongLogo.png" width="500" height="500" alt="logo" />
+        <a
+          className="Main Page"
+          href="../public/index.html"
+        >
+          Enter My Website
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
